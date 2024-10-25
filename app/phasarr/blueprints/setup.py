ba6e9ca05187bd12963b980d4035aff59e471495
Samira <13108552+koninginsamira@users.noreply.github.com>
2024-10-26 00:24:39 +0200

    Added more setup pages

--- app/phasarr/blueprints/setup.py
+++ app/phasarr/blueprints/setup.py
@@ -5,20 +5,23 @@ from flask import Blueprint, flash, redirect, url_for
 
 from phasarr import db, config, catalog
 from phasarr.models.user import User
-from phasarr.forms.setup import SetupForm
+from phasarr.forms.setup import AuthenticationSetupForm, DownloadSetupForm, LibrariesSetupForm
 
 setup_app = Blueprint("setup", __name__)
 
-
 @setup_app.route("/", methods=["GET", "POST"])
 def setup():
+    return redirect(url_for("setup.authentication"))
+
+@setup_app.route("/authentication", methods=["GET", "POST"])
+def authentication():
     user_exists = db.session.execute(sql.select(User)).first()
 
     if not user_exists:
         # if current_user.is_authenticated:
         #     return redirect(url_for("main.main"))
         
-        form: SetupForm = SetupForm()
+        form: AuthenticationSetupForm = AuthenticationSetupForm()
 
         if form.validate_on_submit():
             new_user = User(username=form.username.data)
@@ -29,12 +32,46 @@ def setup():
             config.authentication.method = form.auth_method.data
             
             flash("Setup has been saved!")
-            return redirect(url_for("main.main"))
+            return redirect(url_for("setup.libraries"))
         
         return catalog.render(
-            "setup.Setup",
-            title="Setup",
+            "setup.Authentication",
             form=form
         )
     
     return redirect(url_for("main.main"))
+
+@setup_app.route("/libraries", methods=["GET", "POST"])
+def libraries():
+    form: LibrariesSetupForm = LibrariesSetupForm()
+
+    if form.validate_on_submit():
+        new_libraries = []
+        
+        # User(username=form.username.data)
+        # new_user.set_password(form.password.data)
+        # db.session.add(new_user)
+        # db.session.commit()
+        
+        flash("Libraries have been configured!")
+        return redirect(url_for("setup.download"))
+    
+    return catalog.render(
+        "setup.Libraries",
+        form=form
+    )
+
+@setup_app.route("/download", methods=["GET", "POST"])
+def download():
+    form: DownloadSetupForm = DownloadSetupForm()
+
+    if form.validate_on_submit():
+        # config.authentication.method = form.auth_method.data
+        
+        flash("Download has been configured!")
+        return redirect(url_for("main.main"))
+    
+    return catalog.render(
+        "setup.Download",
+        form=form
+    )
--- app/phasarr/forms/setup.py
+++ app/phasarr/forms/setup.py
@@ -8,15 +8,22 @@ from wtforms.validators import DataRequired, EqualTo
 from phasarr import db
 
 
-class SetupForm(FlaskForm):
+class AuthenticationSetupForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
     auth_method = SelectField("Authentication Method", choices=[("form", "Login Form"), ("prompt", "Browser Prompt")])
-    submit = SubmitField("Save")
+    submit = SubmitField("Continue")
 
     def validate_username(self, username):
         user = db.session.scalar(sql.select(User).where(
             User.username == username.data))
         if user is not None:
             raise ValidationError("Please use a different username.")
+        
+
+class LibrariesSetupForm(FlaskForm):
+    submit = SubmitField("Continue")
+
+class DownloadSetupForm(FlaskForm):
+    submit = SubmitField("Finish")
